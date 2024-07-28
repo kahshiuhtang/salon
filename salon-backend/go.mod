d02f80fb2b60037993fc4f9c6dca9e8c57e47961
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-07-27 22:22:14 -0400

    initial spring boot project

--- salon-backend/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/kahshiuhtang/salon
-
-go 1.22.3
---
+++ salon-backend/src/test/java/com/salon/erp/SalonErpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salon.erp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

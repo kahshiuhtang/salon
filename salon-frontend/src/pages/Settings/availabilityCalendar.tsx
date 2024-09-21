0b721f82965d7e7fba9b7aad252f7fc9b036f8b4
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-09-21 01:32:53 -0400

    dialog to availabilty calendar on timeframe select

--- salon-frontend/src/pages/Settings/availabilityCalendar.tsx
+++ salon-frontend/src/pages/Settings/availabilityCalendar.tsx
@@ -1,4 +1,17 @@
 import { useEffect, useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
@@ -10,11 +23,19 @@ import {
     FormattedAvailability,
     useGetAvailability,
 } from "@/lib/hooks/getCurrentAvailabilities";
-
+import { DateSelectArg } from "@fullcalendar/core/index.js";
+import { CopyIcon } from "lucide-react";
 export default function AvailablityCalendarAST() {
     const [currentAvailabilities, setCurrentAvailabilities] = useState<
         FormattedAvailability[]
     >([]);
+    const [open, setOpen] = useState(false);
+    function handleTimeframeSelect(selectInfo: DateSelectArg) {
+        const { startStr, endStr } = selectInfo;
+        console.log(new Date(startStr));
+        console.log(new Date(endStr));
+        setOpen(true);
+    }
     const navigate = useNavigate();
     const { user } = useUser();
     const { getAvailability } = useGetAvailability();
@@ -46,28 +67,73 @@ export default function AvailablityCalendarAST() {
         }
     }, [user]);
     return (
-        <FullCalendar
-            plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-            headerToolbar={{
-                left: "prev,next today",
-                center: "title",
-                right: "dayGridMonth,timeGridWeek,timeGridDay",
-            }}
-            initialView="dayGridMonth"
-            editable
-            selectable
-            selectMirror
-            dayMaxEvents
-            weekends
-            events={currentAvailabilities}
-            eventContent={renderEventContent}
-            businessHours={{
-                startTime: "9:30",
-                endTime: "21:30",
-            }}
-            slotMinTime="7:00"
-            slotMaxTime="22:00"
-        />
+        <>
+            <FullCalendar
+                plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                headerToolbar={{
+                    left: "prev,next today",
+                    center: "title",
+                    right: "dayGridMonth,timeGridWeek,timeGridDay",
+                }}
+                initialView="dayGridMonth"
+                editable
+                selectable
+                select={handleTimeframeSelect}
+                selectMirror
+                dayMaxEvents
+                weekends
+                events={currentAvailabilities}
+                eventContent={renderEventContent}
+                businessHours={{
+                    startTime: "9:30",
+                    endTime: "21:30",
+                }}
+                slotMinTime="7:00"
+                slotMaxTime="22:00"
+            />
+            <Dialog open={open}>
+                <DialogTrigger asChild>
+                    <Button variant="outline">Share</Button>
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-md">
+                    <DialogHeader>
+                        <DialogTitle>Share link</DialogTitle>
+                        <DialogDescription>
+                            Anyone who has this link will be able to view this.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="flex items-center space-x-2">
+                        <div className="grid flex-1 gap-2">
+                            <Label htmlFor="link" className="sr-only">
+                                Link
+                            </Label>
+                            <Input
+                                id="link"
+                                defaultValue="https://ui.shadcn.com/docs/installation"
+                                readOnly
+                            />
+                        </div>
+                        <Button type="submit" size="sm" className="px-3">
+                            <span className="sr-only">Copy</span>
+                            <CopyIcon className="h-4 w-4" />
+                        </Button>
+                    </div>
+                    <DialogFooter className="sm:justify-start">
+                        <DialogClose asChild>
+                            <Button
+                                type="button"
+                                variant="secondary"
+                                onClick={() => {
+                                    setOpen(false);
+                                }}
+                            >
+                                Close
+                            </Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </>
     );
 }
 function renderEventContent(eventInfo: any) {

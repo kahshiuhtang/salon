305c79e25680ef5d9c600e94ef0e4d4f1f857242
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-09-23 14:53:05 -0400

    new hook for getting employee info from id

--- salon-frontend/src/lib/hooks/getUserInfo.ts
+++ salon-frontend/src/lib/hooks/getUserInfo.ts
@@ -1,6 +1,8 @@
 import { doc, getDoc } from "firebase/firestore";
 import { firebaseDb } from "@/lib/firebase";
 import { SalonUser } from "@/lib/hooks/createProfile";
+import { SalonRole } from "@/lib/types/types";
+
 interface GetNameFromUserIdProps {
     userId: string;
 }
@@ -8,10 +10,29 @@ interface GetNameFromUserIdReturn {
     firstName: string;
     lastName: string;
 }
+
+interface GetEmployeeFromUserIdProps {
+    userId: string;
+}
+interface GetEmployeeFromUserIdReturn {
+    firstName: string;
+    lastName: string;
+    role: SalonRole;
+}
+export interface SalonEmployee {
+    id: string;
+    firstName: string;
+    lastName: string;
+    role: SalonRole;
+}
+
 interface GetUserInfoReturn {
     getNameFromId: (
         props: GetNameFromUserIdProps
     ) => Promise<GetNameFromUserIdReturn>;
+    getEmployeeFromId: (
+        props: GetEmployeeFromUserIdProps
+    ) => Promise<GetEmployeeFromUserIdReturn>;
 }
 export const useGetUserInfo = (): GetUserInfoReturn => {
     const getNameFromId = async (props: GetNameFromUserIdProps) => {
@@ -22,9 +43,25 @@ export const useGetUserInfo = (): GetUserInfoReturn => {
         const userDoc = doc(firebaseDb, "users", userId);
         const userSnapshot = await getDoc(userDoc);
 
-        if (!userSnapshot.exists()) throw new Error("use does not exist");
+        if (!userSnapshot.exists()) throw new Error("user does not exist");
         const data = userSnapshot.data() as SalonUser;
         return { firstName: data.firstName, lastName: data.lastName };
     };
-    return { getNameFromId };
+    const getEmployeeFromId = async (props: GetEmployeeFromUserIdProps) => {
+        if (!props || !props.userId) {
+            throw new Error("arguments invalid");
+        }
+        const userId = props.userId;
+        const userDoc = doc(firebaseDb, "employees", userId);
+        const userSnapshot = await getDoc(userDoc);
+
+        if (!userSnapshot.exists()) throw new Error("user does not exist");
+        const data = userSnapshot.data() as SalonUser;
+        return {
+            firstName: data.firstName,
+            lastName: data.lastName,
+            role: data.role,
+        };
+    };
+    return { getNameFromId, getEmployeeFromId };
 };

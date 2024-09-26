8a6817dac80eb560f40caf9dea5027d29370b567
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-09-26 16:14:18 -0400

    add hooks for getting apps before/after NOW

--- salon-frontend/src/lib/hooks/useAppointment.ts
+++ salon-frontend/src/lib/hooks/useAppointment.ts
@@ -9,8 +9,26 @@ import {
     addDoc,
     updateDoc,
 } from "firebase/firestore";
-import { Appointment, AppointmentState, FullCalendarAppointment } from "@/lib/types/types";
+import {
+    Appointment,
+    AppointmentState,
+    FullCalendarAppointment,
+    SalonRole,
+} from "@/lib/types/types";
 import { firebaseDb } from "@/lib/firebase";
+interface GetAllAppointmentsProps {
+    userId: string;
+}
+interface GetPreviousAppointmentsProp {
+    userId: string;
+    userFirstName: string;
+    role: SalonRole;
+}
+interface GetFutureAppointmentsProp {
+    userId: string;
+    userFirstName: string;
+    role: SalonRole;
+}
 interface GetAllAppointmentsProps {
     userId: string;
 }
@@ -44,6 +62,12 @@ interface UseAppointmentReturn {
     updateAppointmentStatus: (
         statusProps: UpdateAppointmentStatusProps
     ) => Promise<void>;
+    getPreviousAppointments: (
+        props: GetPreviousAppointmentsProp
+    ) => Promise<Appointment[]>;
+    getFutureAppointments: (
+        props: GetFutureAppointmentsProp
+    ) => Promise<Appointment[]>;
 }
 export const useAppointment = (): UseAppointmentReturn => {
     const appCollectionRef = collection(firebaseDb, "appointments");
@@ -110,7 +134,6 @@ export const useAppointment = (): UseAppointmentReturn => {
             return appointments;
         }
     };
-
     const formatAppointments = (appointments: Appointment[]) => {
         var ans: FullCalendarAppointment[] = [];
         for (var i = 0; i < appointments.length; i++) {
@@ -139,16 +162,193 @@ export const useAppointment = (): UseAppointmentReturn => {
         return ans;
     };
     const updateAppointmentStatus = async (
-            statusProps: UpdateAppointmentStatusProps
-        ) => {
-            try {
-                const docRef = doc(firebaseDb, "appointments", statusProps.id);
-                await updateDoc(docRef, {
-                    state: statusProps.newStatus,
-                });
-            } catch (e) {
-                console.log(e);
-            }
-        };
-    return { addAppointment, getAppointments, formatAppointments,updateAppointmentStatus };
+        statusProps: UpdateAppointmentStatusProps
+    ) => {
+        try {
+            const docRef = doc(firebaseDb, "appointments", statusProps.id);
+            await updateDoc(docRef, {
+                state: statusProps.newStatus,
+            });
+        } catch (e) {
+            console.log(e);
+        }
+    };
+    const getPreviousAppointments = async (
+        props: GetPreviousAppointmentsProp
+    ) => {
+        if (!props || !props.userId || !props.role || !props.userFirstName) {
+            throw new Error("Arguments invalid");
+        }
+
+        const userId = props.userId;
+        const userRole = props.role;
+
+        const appointments = new Set<Appointment>();
+        const now = new Date();
+        if (userRole === "ADMIN" || userRole == "MOD") {
+            const appointmentsRef = collection(firebaseDb, "appointments");
+
+            const query1 = query(
+                appointmentsRef,
+                where("tech1", "==", props.userFirstName),
+                where("time", "<", now)
+            );
+            const query2 = query(
+                appointmentsRef,
+                where("tech2", "==", props.userFirstName),
+                where("time", "<", now)
+            );
+            const query3 = query(
+                appointmentsRef,
+                where("tech3", "==", props.userFirstName),
+                where("time", "<", now)
+            );
+            const query4 = query(
+                appointmentsRef,
+                where("tech4", "==", props.userFirstName),
+                where("time", "<", now)
+            );
+
+            const [snapshot1, snapshot2, snapshot3, snapshot4] =
+                await Promise.all([
+                    getDocs(query1),
+                    getDocs(query2),
+                    getDocs(query3),
+                    getDocs(query4),
+                ]);
+            snapshot1.forEach((doc) =>
+                appointments.add({
+                    id: doc.id,
+                    ...doc.data(),
+                } as Appointment)
+            );
+            snapshot2.forEach((doc) =>
+                appointments.add({
+                    id: doc.id,
+                    ...doc.data(),
+                } as Appointment)
+            );
+            snapshot3.forEach((doc) =>
+                appointments.add({
+                    id: doc.id,
+                    ...doc.data(),
+                } as Appointment)
+            );
+            snapshot4.forEach((doc) =>
+                appointments.add({
+                    id: doc.id,
+                    ...doc.data(),
+                } as Appointment)
+            );
+            const result = Array.from(appointments);
+            return result;
+        } else {
+            var appointmentsQuery = query(
+                collection(firebaseDb, "appointments"),
+                where("userId", "==", userId),
+                where("time", "<", now)
+            );
+            const querySnapshot = await getDocs(appointmentsQuery);
+            const appointments: Appointment[] = [];
+            querySnapshot.forEach((doc) => {
+                appointments.push({
+                    id: doc.id,
+                    ...doc.data(),
+                } as Appointment);
+            });
+            return appointments;
+        }
+    };
+    const getFutureAppointments = async (props: GetFutureAppointmentsProp) => {
+        if (!props || !props.userId || !props.role || !props.userFirstName) {
+            throw new Error("Arguments invalid");
+        }
+
+        const userId = props.userId;
+        const userRole = props.role;
+
+        const appointments = new Set<Appointment>();
+        const now = new Date();
+        if (userRole === "ADMIN" || userRole == "MOD") {
+            const appointmentsRef = collection(firebaseDb, "appointments");
+
+            const query1 = query(
+                appointmentsRef,
+                where("tech1", "==", props.userFirstName),
+                where("time", ">", now)
+            );
+            const query2 = query(
+                appointmentsRef,
+                where("tech2", "==", props.userFirstName),
+                where("time", ">", now)
+            );
+            const query3 = query(
+                appointmentsRef,
+                where("tech3", "==", props.userFirstName),
+                where("time", ">", now)
+            );
+            const query4 = query(
+                appointmentsRef,
+                where("tech4", "==", props.userFirstName),
+                where("time", ">", now)
+            );
+
+            const [snapshot1, snapshot2, snapshot3, snapshot4] =
+                await Promise.all([
+                    getDocs(query1),
+                    getDocs(query2),
+                    getDocs(query3),
+                    getDocs(query4),
+                ]);
+            snapshot1.forEach((doc) =>
+                appointments.add({
+                    id: doc.id,
+                    ...doc.data(),
+                } as Appointment)
+            );
+            snapshot2.forEach((doc) =>
+                appointments.add({
+                    id: doc.id,
+                    ...doc.data(),
+                } as Appointment)
+            );
+            snapshot3.forEach((doc) =>
+                appointments.add({
+                    id: doc.id,
+                    ...doc.data(),
+                } as Appointment)
+            );
+            snapshot4.forEach((doc) =>
+                appointments.add({
+                    id: doc.id,
+                    ...doc.data(),
+                } as Appointment)
+            );
+            const result = Array.from(appointments);
+            return result;
+        } else {
+            var appointmentsQuery = query(
+                collection(firebaseDb, "appointments"),
+                where("userId", "==", userId),
+                where("time", ">", now)
+            );
+            const querySnapshot = await getDocs(appointmentsQuery);
+            const appointments: Appointment[] = [];
+            querySnapshot.forEach((doc) => {
+                appointments.push({
+                    id: doc.id,
+                    ...doc.data(),
+                } as Appointment);
+            });
+            return appointments;
+        }
+    };
+    return {
+        addAppointment,
+        getAppointments,
+        formatAppointments,
+        updateAppointmentStatus,
+        getPreviousAppointments,
+        getFutureAppointments,
+    };
 };

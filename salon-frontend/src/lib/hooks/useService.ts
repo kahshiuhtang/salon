792170f3a592bd737d760885c4977ed2983cc411
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-10-02 23:14:25 -0400

    add id to getGoods and getServices from doc

--- salon-frontend/src/lib/hooks/useService.ts
+++ salon-frontend/src/lib/hooks/useService.ts
@@ -6,12 +6,12 @@ import { collection } from "firebase/firestore";
 import { getDocs } from "firebase/firestore";
 import { updateDoc } from "firebase/firestore";
 import { setDoc } from "firebase/firestore";
-interface DefaultServiceProp{
+interface DefaultServiceProp {
     userId: string;
     serviceId?: string;
     service: SalonService;
 }
-interface DefaultGoodProp{
+interface DefaultGoodProp {
     userId: string;
     goodId: string;
     good: SalonGood;
@@ -26,30 +26,48 @@ interface UseServiceReturn {
 }
 export const useService = (): UseServiceReturn => {
     const getServices = async (): Promise<SalonService[]> => {
-        const servicesRef = collection(firebaseDb,  "services");
+        const servicesRef = collection(firebaseDb, "services");
         const servicesSnapshot = await getDocs(servicesRef);
-        const servicesList = servicesSnapshot.docs.map(doc => doc.data() as SalonService);
+        const servicesList = servicesSnapshot.docs.map(
+            (doc) =>
+                ({
+                    ...doc.data(),
+                    id: doc.id,
+                } as SalonService)
+        );
         return servicesList;
     };
 
     const getGoods = async (): Promise<SalonGood[]> => {
-        const goodsRef = collection(firebaseDb,"goods");
+        const goodsRef = collection(firebaseDb, "goods");
         const goodsSnapshot = await getDocs(goodsRef);
-        const goodsList = goodsSnapshot.docs.map(doc => doc.data() as SalonGood);
+        const goodsList = goodsSnapshot.docs.map(
+            (doc) =>
+                ({
+                    ...doc.data(),
+                    id: doc.id,
+                } as SalonGood)
+        );
         return goodsList;
     };
 
-    const modifyService = async ({ serviceId, service }: DefaultServiceProp): Promise<void> => {
+    const modifyService = async ({
+        serviceId,
+        service,
+    }: DefaultServiceProp): Promise<void> => {
         if (serviceId) {
-            const serviceDocRef = doc(firebaseDb,  "services", serviceId);
+            const serviceDocRef = doc(firebaseDb, "services", serviceId);
             await updateDoc(serviceDocRef, { ...service });
         } else {
-            const servicesRef = collection(firebaseDb,  "services");
+            const servicesRef = collection(firebaseDb, "services");
             await setDoc(doc(servicesRef), { ...service });
         }
     };
 
-    const modifyGood = async ({ goodId, good }: DefaultGoodProp): Promise<void> => {
+    const modifyGood = async ({
+        goodId,
+        good,
+    }: DefaultGoodProp): Promise<void> => {
         if (goodId) {
             const goodDocRef = doc(firebaseDb, "goods", goodId);
             await updateDoc(goodDocRef, { ...good });
@@ -59,18 +77,27 @@ export const useService = (): UseServiceReturn => {
         }
     };
 
-    const removeService = async ({ serviceId }: DefaultServiceProp): Promise<void> => {
+    const removeService = async ({
+        serviceId,
+    }: DefaultServiceProp): Promise<void> => {
         if (serviceId) {
             const serviceDocRef = doc(firebaseDb, "services", serviceId);
             await deleteDoc(serviceDocRef);
         }
     };
 
-    const removeGood = async ({  goodId }: DefaultGoodProp): Promise<void> => {
+    const removeGood = async ({ goodId }: DefaultGoodProp): Promise<void> => {
         if (goodId) {
             const goodDocRef = doc(firebaseDb, "goods", goodId);
             await deleteDoc(goodDocRef);
         }
     };
-    return { getServices, getGoods, modifyGood, modifyService, removeService, removeGood };
+    return {
+        getServices,
+        getGoods,
+        modifyGood,
+        modifyService,
+        removeService,
+        removeGood,
+    };
 };

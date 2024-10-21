894b2d992e609f0cd1d08dab03f2c8cfff17db9f
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-10-21 16:26:12 -0400

    format

--- salon-frontend/src/pages/Calendar/calendarSchedule.tsx
+++ salon-frontend/src/pages/Calendar/calendarSchedule.tsx
@@ -8,11 +8,11 @@ import { useUser } from "@clerk/clerk-react";
 import { useNavigate } from "react-router-dom";
 import { useUserProfile } from "@/lib/hooks/useUserProfile";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTrigger,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTrigger,
 } from "@/components/ui/dialog";
 import { DialogTitle } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
@@ -20,117 +20,120 @@ import ScheduleForm from "@/pages/Calendar/scheduleForm";
 import { FullCalendarAppointment } from "@/lib/types/types";
 
 export default function CalendarScheduler() {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-  const [currentEvents, setCurrentEvents] = useState<FullCalendarAppointment[]>(
-    []
-  );
-  const navigate = useNavigate();
-  const { user } = useUser();
-  const { verifyProfile } = useUserProfile();
-  const { getAppointments, formatAppointments } = useAppointment();
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+    const [currentEvents, setCurrentEvents] = useState<
+        FullCalendarAppointment[]
+    >([]);
+    const navigate = useNavigate();
+    const { user } = useUser();
+    const { verifyProfile } = useUserProfile();
+    const { getAppointments, formatAppointments } = useAppointment();
 
-  // Fetch appointments, only when userId is available
-  const fetchAppointments = async () => {
-    try {
-      console.log("fetching data...");
-      if (user == null || user == undefined || user["id"] == null) {
-        console.log("No user id");
-        return;
-      }
-      const userId = user["id"];
-      const isVerified = await verifyProfile({ userId });
-      if (!isVerified) {
-        navigate("/create-profile");
-        return;
-      }
-      const appointments = await getAppointments({ userId });
-      const formattedApps = formatAppointments(appointments);
-      setCurrentEvents(formattedApps);
-    } catch (error) {
-      console.error("Error fetching appointments:", error);
-    }
-  };
-  const hasFetched = useRef(false);
-  useEffect(() => {
-    if (!hasFetched.current && user) {
-      fetchAppointments();
-      hasFetched.current = true;
-    }
-  }, [user]);
+    // Fetch appointments, only when userId is available
+    const fetchAppointments = async () => {
+        try {
+            console.log("fetching data...");
+            if (user == null || user == undefined || user["id"] == null) {
+                console.log("No user id");
+                return;
+            }
+            const userId = user["id"];
+            const isVerified = await verifyProfile({ userId });
+            if (!isVerified) {
+                navigate("/create-profile");
+                return;
+            }
+            const appointments = await getAppointments({ userId });
+            const formattedApps = formatAppointments(appointments);
+            setCurrentEvents(formattedApps);
+        } catch (error) {
+            console.error("Error fetching appointments:", error);
+        }
+    };
+    const hasFetched = useRef(false);
+    useEffect(() => {
+        if (!hasFetched.current && user) {
+            fetchAppointments();
+            hasFetched.current = true;
+        }
+    }, [user]);
 
-  function handleDateSelect(selectInfo: any) {
-    const title = prompt("Please enter a new title for your event");
-    const calendarApi = selectInfo.view.calendar;
-    if (title) {
-      console.log("Event Title:", title);
+    function handleDateSelect(selectInfo: any) {
+        const title = prompt("Please enter a new title for your event");
+        const calendarApi = selectInfo.view.calendar;
+        if (title) {
+            console.log("Event Title:", title);
+        }
+        calendarApi.unselect();
     }
-    calendarApi.unselect();
-  }
 
-  function handleEventClick(clickInfo: any) {
-    if (
-      confirm(
-        `Are you sure you want to delete the event '${clickInfo.event.title}'?`
-      )
-    ) {
-      clickInfo.event.remove();
+    function handleEventClick(clickInfo: any) {
+        if (
+            confirm(
+                `Are you sure you want to delete the event '${clickInfo.event.title}'?`
+            )
+        ) {
+            clickInfo.event.remove();
+        }
     }
-  }
-  return (
-    <div className="w-full flex justify-center mt-8">
-      <div className="w-3/4">
-        <FullCalendar
-          plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
-          headerToolbar={{
-            left: "prev,next today",
-            center: "title",
-            right: "dayGridMonth,timeGridWeek,timeGridDay",
-          }}
-          initialView="dayGridMonth"
-          editable
-          selectable
-          selectMirror
-          dayMaxEvents
-          weekends
-          events={currentEvents}
-          select={handleDateSelect}
-          eventContent={renderEventContent}
-          eventClick={handleEventClick}
-          businessHours={{
-            startTime: "9:30",
-            endTime: "21:30",
-          }}
-          slotMinTime="7:00"
-          slotMaxTime="22:00"
-        />
-        <div className="w-full mt-2 flex justify-end">
-          <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-            <DialogTrigger asChild>
-              <Button>Schedule</Button>
-            </DialogTrigger>
-            <DialogContent className="sm:max-w-[425px]">
-              <DialogHeader>
-                <DialogTitle>Create New Appointment</DialogTitle>
-                <DialogDescription>
-                  Schedule an appointment in an available slot. A staff member
-                  will confirm your appointment at a later time.
-                </DialogDescription>
-              </DialogHeader>
-              <ScheduleForm setIsDialogOpen={setIsDialogOpen} />
-            </DialogContent>
-          </Dialog>
+    return (
+        <div className="w-full flex justify-center mt-8">
+            <div className="w-3/4">
+                <FullCalendar
+                    plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                    headerToolbar={{
+                        left: "prev,next today",
+                        center: "title",
+                        right: "dayGridMonth,timeGridWeek,timeGridDay",
+                    }}
+                    initialView="dayGridMonth"
+                    editable
+                    selectable
+                    selectMirror
+                    dayMaxEvents
+                    weekends
+                    events={currentEvents}
+                    select={handleDateSelect}
+                    eventContent={renderEventContent}
+                    eventClick={handleEventClick}
+                    businessHours={{
+                        startTime: "9:30",
+                        endTime: "21:30",
+                    }}
+                    slotMinTime="7:00"
+                    slotMaxTime="22:00"
+                />
+                <div className="w-full mt-2 flex justify-end">
+                    <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+                        <DialogTrigger asChild>
+                            <Button>Schedule</Button>
+                        </DialogTrigger>
+                        <DialogContent className="sm:max-w-[425px]">
+                            <DialogHeader>
+                                <DialogTitle>
+                                    Create New Appointment
+                                </DialogTitle>
+                                <DialogDescription>
+                                    Schedule an appointment in an available
+                                    slot. A staff member will confirm your
+                                    appointment at a later time.
+                                </DialogDescription>
+                            </DialogHeader>
+                            <ScheduleForm setIsDialogOpen={setIsDialogOpen} />
+                        </DialogContent>
+                    </Dialog>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 // Custom event content renderer
 function renderEventContent(eventInfo: any) {
-  return (
-    <>
-      <b>{eventInfo.timeText}</b>
-      <i>{eventInfo.event.title}</i>
-    </>
-  );
+    return (
+        <>
+            <b>{eventInfo.timeText}</b>
+            <i>{eventInfo.event.title}</i>
+        </>
+    );
 }

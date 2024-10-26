6ad9f6b994e8c4d828e1d9f52620a2bf4fe4b9bc
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-10-25 22:42:40 -0400

    formatting

--- salon-frontend/src/pages/Settings/editDeleteDropdown.tsx
+++ salon-frontend/src/pages/Settings/editDeleteDropdown.tsx
@@ -1,94 +1,103 @@
-"use client"
+"use client";
 
-import { useState } from "react"
-import { MoreVertical, Edit, Trash2 } from "lucide-react"
-import { Button } from "@/components/ui/button"
+import { useState } from "react";
+import { MoreVertical, Edit, Trash2 } from "lucide-react";
+import { Button } from "@/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
-  Dialog,
-  DialogContent,
-  DialogHeader,
-  DialogTitle,
-  DialogDescription,
-  DialogFooter,
-  DialogClose,
-} from "@/components/ui/dialog"
+    Dialog,
+    DialogContent,
+    DialogHeader,
+    DialogTitle,
+    DialogDescription,
+    DialogFooter,
+    DialogClose,
+} from "@/components/ui/dialog";
 
-import AvailabilityCard from "@/pages/Settings/availabilityCard"
-import { Availability } from "@/lib/types/types"
+import AvailabilityCard from "@/pages/Settings/availabilityCard";
+import { Availability } from "@/lib/types/types";
 
-interface EditDeleteDropdownProps{
+interface EditDeleteDropdownProps {
     availability: Availability;
 }
-export default function EditDeleteDropdown({ availability }: EditDeleteDropdownProps) {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false)
-  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false)
-  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false)
+export default function EditDeleteDropdown({
+    availability,
+}: EditDeleteDropdownProps) {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+    const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+    const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
 
-  const handleEdit = () => {
-    setIsDropdownOpen(false)
-    setIsEditDialogOpen(true)
-  }
+    const handleEdit = () => {
+        setIsDropdownOpen(false);
+        setIsEditDialogOpen(true);
+    };
 
-  const handleDelete = () => {
-    setIsDropdownOpen(false)
-    setIsDeleteDialogOpen(true)
-  }
+    const handleDelete = () => {
+        setIsDropdownOpen(false);
+        setIsDeleteDialogOpen(true);
+    };
 
-  const confirmDelete = () => {
-    setIsDeleteDialogOpen(false)
-  }
+    const confirmDelete = () => {
+        setIsDeleteDialogOpen(false);
+    };
 
-  return (
-    <div>
-      <DropdownMenu open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" className="h-8 w-8 p-0">
-            <span className="sr-only">Open menu</span>
-            <MoreVertical className="h-4 w-4" />
-          </Button>
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuItem onSelect={handleEdit}>
-            <Edit className="mr-2 h-4 w-4" />
-            <span>Edit</span>
-          </DropdownMenuItem>
-          <DropdownMenuItem onSelect={handleDelete}>
-            <Trash2 className="mr-2 h-4 w-4" />
-            <span>Delete</span>
-          </DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
+    return (
+        <div>
+            <DropdownMenu
+                open={isDropdownOpen}
+                onOpenChange={setIsDropdownOpen}
+            >
+                <DropdownMenuTrigger asChild>
+                    <Button variant="ghost" className="h-8 w-8 p-0">
+                        <span className="sr-only">Open menu</span>
+                        <MoreVertical className="h-4 w-4" />
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end">
+                    <DropdownMenuItem onSelect={handleEdit}>
+                        <Edit className="mr-2 h-4 w-4" />
+                        <span>Edit</span>
+                    </DropdownMenuItem>
+                    <DropdownMenuItem onSelect={handleDelete}>
+                        <Trash2 className="mr-2 h-4 w-4" />
+                        <span>Delete</span>
+                    </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
 
-      <Dialog open={isEditDialogOpen} onOpenChange={setIsEditDialogOpen}>
-        <DialogContent className="sm:max-w-[425px]">
-          <AvailabilityCard availability={availability}/>
-        </DialogContent>
-      </Dialog>
+            <Dialog open={isEditDialogOpen} onOpenChange={setIsEditDialogOpen}>
+                <DialogContent className="sm:max-w-[425px]">
+                    <AvailabilityCard availability={availability} />
+                </DialogContent>
+            </Dialog>
 
-      <Dialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Confirm Deletion</DialogTitle>
-            <DialogDescription>
-              Are you sure you want to delete this item? This action cannot be undone.
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter className="sm:justify-start">
-            <Button variant="destructive" onClick={confirmDelete}>
-              Delete
-            </Button>
-            <DialogClose asChild>
-              <Button variant="outline">Cancel</Button>
-            </DialogClose>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
-  )
+            <Dialog
+                open={isDeleteDialogOpen}
+                onOpenChange={setIsDeleteDialogOpen}
+            >
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Confirm Deletion</DialogTitle>
+                        <DialogDescription>
+                            Are you sure you want to delete this item? This
+                            action cannot be undone.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter className="sm:justify-start">
+                        <Button variant="destructive" onClick={confirmDelete}>
+                            Delete
+                        </Button>
+                        <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogClose>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
 }
--- salon-frontend/src/pages/Settings/listView.tsx
+++ salon-frontend/src/pages/Settings/listView.tsx
@@ -3,137 +3,147 @@
 import { useEffect, useState } from "react";
 import { CalendarIcon, Clock, RepeatIcon } from "lucide-react";
 import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
 } from "@/components/ui/table";
 import { Button } from "@/components/ui/button";
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogFooter,
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogFooter,
 } from "@/components/ui/dialog";
 import { Availability } from "@/lib/types/types";
 import { useAvailability } from "@/lib/hooks/useAvailability";
 import { useUser } from "@clerk/clerk-react";
 import EditDeleteDropdown from "@/pages/Settings/editDeleteDropdown";
+import { Toaster } from "@/components/ui/toaster";
 
 const mockData: Availability[] = [];
 
 export default function AvailabilityListView() {
-  const [availabilities, setAvailabilities] =
-    useState<Availability[]>(mockData);
-  const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
-  const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
-  const [deletingId, setDeletingId] = useState<string | null>(null);
+    const [availabilities, setAvailabilities] =
+        useState<Availability[]>(mockData);
+    const [isEditDialogOpen, setIsEditDialogOpen] = useState(false);
+    const [isDeleteDialogOpen, setIsDeleteDialogOpen] = useState(false);
+    const [deletingId, setDeletingId] = useState<string | null>(null);
 
-  const { getUnformattedAvailability } = useAvailability();
-  const { user } = useUser();
-  var userId = user?.id || "";
+    const { getUnformattedAvailability } = useAvailability();
+    const { user } = useUser();
+    var userId = user?.id || "";
 
-  const getRepeatText = (availability: Availability) => {
-    if (availability.repeatTypeWeekly && availability.repeatTypeDaily)
-      return `Repeats ${availability.repeatTypeWeekly} and ${availability.repeatTypeDaily}`;
-    if (availability.repeatTypeWeekly)
-      return `Repeats ${availability.repeatTypeWeekly}`;
-    if (availability.repeatTypeDaily)
-      return `Repeats ${availability.repeatTypeDaily}`;
-    return "No repeats";
-  };
+    const getRepeatText = (availability: Availability) => {
+        if (availability.repeatTypeWeekly && availability.repeatTypeDaily)
+            return `Repeats ${availability.repeatTypeWeekly} and ${availability.repeatTypeDaily}`;
+        if (availability.repeatTypeWeekly)
+            return `Repeats ${availability.repeatTypeWeekly}`;
+        if (availability.repeatTypeDaily)
+            return `Repeats ${availability.repeatTypeDaily}`;
+        return "No repeats";
+    };
 
-  const confirmDelete = () => {
-    if (deletingId) {
-      setAvailabilities(availabilities.filter((a) => a.id !== deletingId));
-      setIsDeleteDialogOpen(false);
-      setDeletingId(null);
-    }
-  };
+    const confirmDelete = () => {
+        if (deletingId) {
+            setAvailabilities(
+                availabilities.filter((a) => a.id !== deletingId)
+            );
+            setIsDeleteDialogOpen(false);
+            setDeletingId(null);
+        }
+    };
 
-  const fetchAvailability = async function () {
-    const availability = await getUnformattedAvailability({ userId });
-    setAvailabilities(availability);
-  };
-  useEffect(() => {
-    fetchAvailability();
-  }, []);
+    const fetchAvailability = async function () {
+        const availability = await getUnformattedAvailability({ userId });
+        setAvailabilities(availability);
+    };
+    useEffect(() => {
+        fetchAvailability();
+    }, []);
 
-  return (
-    <div className="container mx-auto py-10">
-      <h2 className="text-2xl font-bold mb-4">Availability List</h2>
-      <Table>
-        <TableHeader>
-          <TableRow>
-            <TableHead>Date</TableHead>
-            <TableHead>Time</TableHead>
-            <TableHead>Repeat</TableHead>
-            <TableHead className="text-right">Actions</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {availabilities.map((availability) => (
-            <TableRow key={availability.id}>
-              <TableCell>
-                <div className="flex items-center">
-                  <CalendarIcon className="mr-2 h-4 w-4" />
-                  {availability.date.toDateString()}
-                </div>
-              </TableCell>
-              <TableCell>
-                <div className="flex items-center">
-                  <Clock className="mr-2 h-4 w-4" />
-                  {availability.startTime} - {availability.endTime}
-                </div>
-              </TableCell>
-              <TableCell>
-                <div className="flex items-center">
-                  <RepeatIcon className="mr-2 h-4 w-4" />
-                  {getRepeatText(availability)}
-                </div>
-              </TableCell>
-              <TableCell className="text-right">
-                <EditDeleteDropdown availability={availability}/>
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+    return (
+        <div className="container mx-auto py-10">
+            <Toaster />
+            <h2 className="text-2xl font-bold mb-4">Availability List</h2>
+            <Table>
+                <TableHeader>
+                    <TableRow>
+                        <TableHead>Date</TableHead>
+                        <TableHead>Time</TableHead>
+                        <TableHead>Repeat</TableHead>
+                        <TableHead className="text-right">Actions</TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    {availabilities.map((availability) => (
+                        <TableRow key={availability.id}>
+                            <TableCell>
+                                <div className="flex items-center">
+                                    <CalendarIcon className="mr-2 h-4 w-4" />
+                                    {availability.date.toDateString()}
+                                </div>
+                            </TableCell>
+                            <TableCell>
+                                <div className="flex items-center">
+                                    <Clock className="mr-2 h-4 w-4" />
+                                    {availability.startTime} -{" "}
+                                    {availability.endTime}
+                                </div>
+                            </TableCell>
+                            <TableCell>
+                                <div className="flex items-center">
+                                    <RepeatIcon className="mr-2 h-4 w-4" />
+                                    {getRepeatText(availability)}
+                                </div>
+                            </TableCell>
+                            <TableCell className="text-right">
+                                <EditDeleteDropdown
+                                    availability={availability}
+                                />
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
 
-      <Dialog open={isEditDialogOpen} onOpenChange={setIsEditDialogOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Edit Availability</DialogTitle>
-          </DialogHeader>
-        </DialogContent>
-      </Dialog>
+            <Dialog open={isEditDialogOpen} onOpenChange={setIsEditDialogOpen}>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Edit Availability</DialogTitle>
+                    </DialogHeader>
+                </DialogContent>
+            </Dialog>
 
-      <Dialog open={isDeleteDialogOpen} onOpenChange={setIsDeleteDialogOpen}>
-        <DialogContent>
-          <DialogHeader>
-            <DialogTitle>Confirm Deletion</DialogTitle>
-            <DialogDescription>
-              Are you sure you want to delete this availability? This action
-              cannot be undone.
-            </DialogDescription>
-          </DialogHeader>
-          <DialogFooter>
-            <Button
-              variant="outline"
-              onClick={() => setIsDeleteDialogOpen(false)}
+            <Dialog
+                open={isDeleteDialogOpen}
+                onOpenChange={setIsDeleteDialogOpen}
             >
-              Cancel
-            </Button>
-            <Button variant="destructive" onClick={confirmDelete}>
-              Delete
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
-    </div>
-  );
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>Confirm Deletion</DialogTitle>
+                        <DialogDescription>
+                            Are you sure you want to delete this availability?
+                            This action cannot be undone.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <DialogFooter>
+                        <Button
+                            variant="outline"
+                            onClick={() => setIsDeleteDialogOpen(false)}
+                        >
+                            Cancel
+                        </Button>
+                        <Button variant="destructive" onClick={confirmDelete}>
+                            Delete
+                        </Button>
+                    </DialogFooter>
+                </DialogContent>
+            </Dialog>
+        </div>
+    );
 }

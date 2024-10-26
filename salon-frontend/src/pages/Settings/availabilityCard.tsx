7510b85cff784fcaf1b889c91291f1b9ad21a84d
Kah Shiuh Tang <kahshiuh.tang@gmail.com>
2024-10-25 22:36:32 -0400

    remove excess toaster

--- salon-frontend/src/pages/Settings/availabilityCard.tsx
+++ salon-frontend/src/pages/Settings/availabilityCard.tsx
@@ -6,7 +6,6 @@ import {
     CardTitle,
 } from "@/components/ui/card";
 import AvailabilityForm from "@/pages/Settings/availabilityForm";
-import { Toaster } from "@/components/ui/toaster"
 import { Availability } from "@/lib/types/types";
 interface AvailabilityCardProps{
     availability?: Availability;
@@ -14,7 +13,6 @@ interface AvailabilityCardProps{
 export default function AvailabilityCard({availability} : AvailabilityCardProps) {
     return (
         <div className="w-full mt-2">
-            <Toaster/>
             <Card className={"w-full"}>
                 <CardHeader className="pl-8 pt-8 pb-0 mb-2">
                     <CardTitle>Create Availability Block</CardTitle>
